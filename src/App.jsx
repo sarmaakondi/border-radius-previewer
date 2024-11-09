d21aa51dd17b9c46d8006399aa05d42053a416c4
sarmaakondi <avnmsarma@gmail.com>
2024-11-09 19:25:24 +1100

    Setup tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import appLogo from "./assets/icon.svg";
 import "./App.css";
 
 function App() {
     return (
-        <div>
-            <img src={appLogo} alt="app logo" style={{ maxWidth: "60px" }} />
-            <h1>Border Radius Previewer</h1>
+        <div className="flex items-center justify-center px-4 py-8 md:px-8 md:py-12 lg:px-12 lg:py-16">
+            <h1 className="text-4xl font-mono mt-4">Border Radius Previewer</h1>
         </div>
     );
 }
