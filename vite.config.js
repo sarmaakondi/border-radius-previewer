32e2c403a9ca1284cdeac2bcd3875a7026520136
sarmaakondi <avnmsarma@gmail.com>
2024-11-10 09:52:42 +1100

    Apply optimizations, purge unused CSS and apply JS minification

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import viteCompression from "vite-plugin-compression";
+import pluginPurgeCss from "vite-plugin-purgecss-updated-v5";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,5 +12,10 @@ export default defineConfig({
             threshold: 1025,
             verbose: true,
         }),
+        pluginPurgeCss({ variables: true }),
     ],
+    build: {
+        minify: "terser",
+        terserOptions: { compress: { drop_console: true } },
+    },
 });
