b4106f34dc4e83464830e9564ecfc047d6ac3ff1
sarmaakondi <avnmsarma@gmail.com>
2024-11-09 20:29:03 +1100

    Define responsive width and height for BorderBox component

--- src/BorderBox.jsx
+++ src/BorderBox.jsx
@@ -1,5 +1,9 @@
 function BorderBox() {
-    return <h1>Border Box</h1>;
+    return (
+        <div className="flex items-center justify-center mt-4">
+            <div className="border-4 border-gray-500 w-60 h-60 md:w-80 md:h-80 lg:w-96 lg:h-96"></div>
+        </div>
+    );
 }
 
 export default BorderBox;
