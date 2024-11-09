4c7ba9346226f8fd19c4ab3fe09a30f176485734
sarmaakondi <avnmsarma@gmail.com>
2024-11-09 21:28:07 +1100

    Create state to hold the border radius individual values

--- src/BorderBox.jsx
+++ src/BorderBox.jsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
+
 function BorderBox() {
+    const [borderRadius, setBorderRadius] = useState({
+        topLeft: 0,
+        topRight: 0,
+        bottomLeft: 0,
+        bottomRight: 0,
+    });
+
     return (
         <div className="flex items-center justify-center mt-4">
             <div className="border-4 border-gray-500 w-80 h-60 md:w-120 md:h-80 lg:w-140 lg:h-100"></div>
