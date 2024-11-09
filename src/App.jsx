c44db3b8e420d49cd81ae57df07f562da3c7adc4
sarmaakondi <avnmsarma@gmail.com>
2024-11-09 20:11:41 +1100

    Move the header content into Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import "./App.css";
+import Header from "./Header";
 
 function App() {
     return (
         <div className="flex items-center justify-center px-4 py-8 md:px-8 md:py-12 lg:px-12 lg:py-16">
-            <span className="text-2xl md:text-3xl lg:text-4xl font-mono p-2 md:p-4 lg:p-6 border-l-4 border-t-4 rounded-l-2xl border-purple-400">
-                Border
-            </span>
-            <span className="text-2xl md:text-3xl lg:text-4xl font-mono">
-                Radius
-            </span>
-            <span className="text-2xl md:text-3xl lg:text-4xl font-mono p-2 md:p-4 lg:p-6 border-r-4 border-b-4 rounded-r-2xl border-yellow-400">
-                Previewer
-            </span>
+            <Header />
         </div>
     );
 }
---
+++ src/Header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+    return (
+        <>
+            <span className="text-2xl md:text-3xl lg:text-4xl font-mono p-2 md:p-4 lg:p-6 border-l-4 border-t-4 rounded-l-2xl border-purple-400">
+                Border
+            </span>
+            <span className="text-2xl md:text-3xl lg:text-4xl font-mono">
+                Radius
+            </span>
+            <span className="text-2xl md:text-3xl lg:text-4xl font-mono p-2 md:p-4 lg:p-6 border-r-4 border-b-4 rounded-r-2xl border-yellow-400">
+                Previewer
+            </span>
+        </>
+    );
+}
+
+export default Header;
