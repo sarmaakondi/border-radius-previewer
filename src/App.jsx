9650439a9127a4fab01db1c433dee2dd30a4fdbc
sarmaakondi <avnmsarma@gmail.com>
2024-11-09 19:38:39 +1100

    Create app logo/header

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import "./App.css";
 function App() {
     return (
         <div className="flex items-center justify-center px-4 py-8 md:px-8 md:py-12 lg:px-12 lg:py-16">
-            <h1 className="text-4xl font-mono mt-4">Border Radius Previewer</h1>
+            <span className="text-4xl font-mono p-6 border-l-4 border-t-4 rounded-l-2xl border-purple-400">
+                Border
+            </span>
+            <span className="text-4xl font-mono">Radius</span>
+            <span className="text-4xl font-mono p-6 border-r-4 border-b-4 rounded-r-2xl border-yellow-400">
+                Previewer
+            </span>
         </div>
     );
 }
