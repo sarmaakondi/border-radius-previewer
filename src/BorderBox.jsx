460e6d3d87f4b4444d9c3414fdaaddbd5573adbd
sarmaakondi <avnmsarma@gmail.com>
2024-11-09 21:19:45 +1100

    Extend tailwind spacing configuration to make the box responsive across screens

--- src/BorderBox.jsx
+++ src/BorderBox.jsx
@@ -1,7 +1,7 @@
 function BorderBox() {
     return (
         <div className="flex items-center justify-center mt-4">
-            <div className="border-4 border-gray-500 w-60 h-60 md:w-80 md:h-80 lg:w-96 lg:h-96"></div>
+            <div className="border-4 border-gray-500 w-80 h-60 md:w-120 md:h-80 lg:w-140 lg:h-100"></div>
         </div>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,17 @@ export default {
         "./src/styles/globals.css",
     ],
     theme: {
-        extend: {},
+        extend: {
+            spacing: {
+                80: "20rem",
+                100: "25rem",
+                120: "30rem",
+                140: "35rem",
+                160: "40rem",
+                180: "45rem",
+                200: "50rem",
+            },
+        },
     },
     plugins: [],
 };
